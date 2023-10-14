af449cf564f7f5c55f5f48ac45c31d7188331d2e
khairulimran <info@khairulimran.com>
2023-10-14 15:45:35 +0800

    Add middleware admin

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -28,8 +30,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('categories', CategoryController::class)
+    ->middleware('is_admin');
 });
 
 require __DIR__.'/auth.php';
 
-Route::resource('categories', CategoryController::class);
+
